df5c09d187b90d299a5c8063a86642fe59aea7bb
Ryan Hartzell <ryanhartzell115@gmail.com>
2017-11-30 18:56:32 -0800

    stuff

--- InnReservations.java
+++ InnReservations.java
@@ -7,89 +7,100 @@ import java.util.*;
 import java.sql.*;
 
 class InnReservations {	
-   public static void main (String args []) throws FileNotFoundException {
-      Scanner scan = new Scanner(new File("ServerSettings.txt"));
-      Console console;
-      Connection conn = null;
-      ResultSet rset = null;
-      PreparedStatement pstmt = null;
-      String query = null, line = null;
-      String url = scan.nextLine();	
-      String userID = scan.nextLine();
-      String pword = scan.nextLine();   	
-      int i = 0;
+    public static void main (String args []) throws FileNotFoundException {
+        Scanner scan = new Scanner(new File("ServerSettings.txt"));
+        Console console;
+        Connection conn = null;
+        ResultSet rset = null;
+        PreparedStatement pstmt = null;
+        String query = null, line = null;
+        String url = scan.nextLine();	
+        String userID = scan.nextLine();
+        String pword = scan.nextLine();
+        boolean isFull;
+        int i = 0;
 
-      try {
-	 // Load the mysql JDBC driver
-	 Class.forName("com.mysql.jdbc.Driver").newInstance();
-	 System.out.println ("Driver class found and loaded.");
+        try {
+            // Load the mysql JDBC driver
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            System.out.println ("Driver class found and loaded.");
 
-	 // now connect to mySQL with user's own database
-	 System.out.println ("Connecting...");
-	 conn =  DriverManager.getConnection(
-	    url + "?"
-	    + "user=" + userID + "&password=" + pword);
-	 System.out.println ("connected.");
+            // now connect to mySQL with user's own database
+            System.out.println ("Connecting...");
+            conn =  DriverManager.getConnection(
+            url + "?"
+            + "user=" + userID + "&password=" + pword);
+            System.out.println ("connected.");
 
-	 // now we're going to create a table and populate it
-	 try {
-	    // put our CREATE TABLE statement into a String
-	    String table = "CREATE TABLE rooms("
-               + "id char(5) PRIMARY KEY,"
-               +"name char(40),"
-               +"beds integer,"
-               +"bedtype char(10),"
-               +"maxOccupancy integer,"
-               +"basePrice float,"
-               +"decor char(15));";
-          
-	    // here we get our Statement and execute our CREATE TABLE statement
-	    Statement s1 = conn.createStatement();
-	    s1.executeUpdate(table);
-         }
-         catch (Exception ee) {
-	    System.out.println("ee96: " + ee);
-	 }
-         try{
-              String table2 = "CREATE TABLE reservations("
-               +"id integer PRIMARY KEY,"
-               +"room char(5) REFERENCES rooms(id),"
-               +"checkin date,"
-               +"checkout date,"
-               +"rate float,"
-               +"lastname char(20),"
-               +"firstname char(20),"
-               +"adults integer,"
-               +"kids integer);";
+            // now we're going to create a table and populate it
+            try {
+                // put our CREATE TABLE statement into a String
+                String table = "CREATE TABLE rooms("
+                + "id char(5) PRIMARY KEY,"
+                +"name char(40),"
+                +"beds integer,"
+                +"bedtype char(10),"
+                +"maxOccupancy integer,"
+                +"basePrice float,"
+                +"decor char(15));";
 
-	    // here we get our Statement and execute our CREATE TABLE statement
-	   Statement s2 = conn.createStatement();
-	    s2.executeUpdate(table2);
+                // here we get our Statement and execute our CREATE TABLE statement
+                Statement s1 = conn.createStatement();
+                s1.executeUpdate(table);
+            }
+            catch (Exception ee) {
+                System.out.println("ee96: " + ee);
+            }
+            try{
+                String table2 = "CREATE TABLE reservations("
+                +"id integer PRIMARY KEY,"
+                +"room char(5) REFERENCES rooms(id),"
+                +"checkin date,"
+                +"checkout date,"
+                +"rate float,"
+                +"lastname char(20),"
+                +"firstname char(20),"
+                +"adults integer,"
+                +"kids integer);";
 
-	 }
-	 catch (Exception ee) {
-	    System.out.println("ee96: " + ee);
-	 }
+            // here we get our Statement and execute our CREATE TABLE statement
+            Statement s2 = conn.createStatement();
+            s2.executeUpdate(table2);
+            }
+            catch (Exception ee) {
+                System.out.println("ee96: " + ee);
+            }
 
-	} 
-      catch (Exception ex) {
-	 ex.printStackTrace( );
-      }
-      finally {
-	 try {
-	    pstmt.close( );
-	 }
-	 catch (Exception e){}
-	 try {
-	    rset.close( );
-	 }
-	 catch (Exception e){}
-	 try {
-	    conn.close( );
-	 }
-	 catch (Exception e){}	
-      } // end finally   	
+            query = "select COUNT(*) from rooms";
+            pstmt = conn.prepareStatement(query);
+            rset = pstmt.executeQuery();   // NO PARAMETER NEEDED
+            rset.next( );
+            isFull = rset.getInt("COUNT(*)") >= 10;
 
-   } // end main
+            query = "select COUNT(*) from reservations";
+            pstmt = conn.prepareStatement(query);
+            rset = pstmt.executeQuery();   // NO PARAMETER NEEDED
+            rset.next( );
+            isFull = isFull && rset.getInt("COUNT(*)") >= 600;
 
+            System.out.println(isFull);
+        } 
+        catch (Exception ex) {
+            ex.printStackTrace( );
+        }
+        finally {
+            try {
+                pstmt.close( );
+            }
+            catch (Exception e){}
+            try {
+                rset.close( );
+            }
+            catch (Exception e){}
+            try {
+                conn.close( );
+            }
+            catch (Exception e){}	
+        } // end finally   	
+    } // end main
 } // end class
